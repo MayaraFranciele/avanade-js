dfb82ec1ef5716fbc60885273ac1ce1319737ca8
yayahsilva <mayara.franciele39@gmail.com>
2023-07-04 18:39:12 -0300

    Recursos Nativos

---
+++ Aula 1/script.js
@@ -0,0 +1,17 @@
+//Comentário em Javascript de uma linha
+        /*comentário de mais de uma linha*/
+
+//RECURSOS NATIVOS
+
+//Console
+
+console.log('Boa noite pessoal!')
+console.info('Console de informação.')
+console.warn('Cuidado!')
+console.error('Aqui temos uma mensagem de erro.')
+
+//Caixas de diálogo:
+
+window.alert('Janela de alerta!')//Janela de alerta
+window.confirm('Tudo bem pessoal?')//Janela de confirmação
+window.prompt('Qual seu nome?')//Janela para obter algum dado do usuário
